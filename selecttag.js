896a14339fdfae961268a50d6c1bd8dc72d1e593
abhi-54 <71889182+abhi-54@users.noreply.github.com>
2021-03-27 18:49:16 +0530

    added js

--- selecttag.js
+++ selecttag.js
@@ -34,20 +34,3 @@ function diseasedisplay(select)
   }
 }
 
-var sliderIndex = [1,1,1];
-var sliderId = ["mySlides1", "mySlides2", "mySlides3"]
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-    
-function plusSlides(n, no) {showSlides(sliderIndex[no] += n, no);}
-   
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(sliderId[no]);
-  if (n > x.length) {sliderIndex[no] = 1}    
-  if (n < 1) {sliderIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "block";}
-  x[sliderIndex[no]-1].style.display = "block";  
-}
